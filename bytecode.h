d77f320fcee1395b237c3df2caaf7e0002fce050
anhcx0209 <anhcx0209@gmail.com>
2016-07-09 23:46:51 +0300

    Create function to add 1, 2, 4 bytes to byte list

--- bytecode.h
+++ bytecode.h
@@ -4,8 +4,44 @@
 #include <stdbool.h>
 #include "bytecode_struct.h"
 
+void generateByteCode() {
+	main_code = createByteList();
 
+	// MAGIC NUMBER
 
+}
+
+struct ByteList *createByteList() {
+	struct ByteList* code = (struct ByteList*) malloc(sizeof(struct ByteList));
+	code->first = code->last = NULL;
+	code->size = 0;
+	
+	return code;
+}
+
+void appendByte(struct ByteList *list, uint8_t data) {
+	// create new byte element
+	struct ByteElement *e = (struct ByteElement *)malloc(sizeof(struct ByteElement));
+	e->data = data;
+	e->next = NULL;
+
+	// add to list
+	if (list->first == NULL) {
+		list->first = list->last = e;
+	} else {
+		list->last->next = e;
+		list->last = e;
+	}
+	list->size++;
+}
+
+void append2Bytes(struct ByteList *list, uint16_t data) {
+
+}
+
+void append4Bytes(struct ByteList *list, uint32_t data) {
+
+}
 
 
 
--- bytecode_struct.h
+++ bytecode_struct.h
@@ -1,5 +1,32 @@
 #ifndef _BYTE_CODE_STRUCT_H_
 #define _BYTE_CODE_STRUCT_H_
 
+#include <stdint.h>
+
+struct ByteElement{
+	uint8_t data;
+	struct ByteElement* next;
+};
+
+struct ByteList{
+	struct ByteElement* first;
+	struct ByteElement* last;
+	int size;
+};
+
+
+
+
+// ----------------- GLOBAL VARIABLE -----------------------
+struct ByteList *main_code;
+
+void generateByteCode();
+struct ByteList *createByteList();
+
+
+void appendByte(struct ByteList *list, uint8_t data);
+void append2Bytes(struct ByteList *list, uint16_t data);
+void append4Bytes(struct ByteList *list, uint32_t data);
+
 
 #endif // BYTE_CODE_STRUCT_H
